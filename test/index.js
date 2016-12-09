8c1f4e036d49e4c0a8f7094d35bc7fd0d4c91195
刘祺 <qil@jumei.com>
2016-12-09 17:34:26 +0800

    up coverage

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 var describe = require('mocha').describe;
+var before = require('mocha').before;
 var after = require('mocha').after;
 var it = require('mocha').it;
 var assert = require('assert');
@@ -8,6 +9,19 @@ var exec = require('../');
 var Promise = require('any-promise');
 
 describe('git path', function() {
+	before(function() {
+		process.versions.electron = 0.14;
+	});
+
+	if(process.version.replace(/^.*?(\d+).*?$/, '$1') > 6) {
+		it('exec.ls', function() {
+			return exec.ls().then(function(result) {
+				result = result[0].split(/\r?\n/).filter(Boolean).sort();
+				assert.ok(result.indexOf('package.json') >= 0);
+				assert.ok(result.indexOf('README.md') >= 0);
+			});
+		});
+	}
 
 	it('ls', function() {
 		return exec('ls').then(function(result) {
