d1fb8c2aac9a3941c66bea0d2e9ba595926b88ab
刘祺 <gucong@gmail.com>
2017-10-12 11:49:17 +0800

    ncu -a

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+'use strict';
 var describe = require('mocha').describe;
 var it = require('mocha').it;
 var assert = require('assert');
@@ -6,36 +7,33 @@ var fs = require('mz/fs');
 var exec = require('../');
 var Promise = require('any-promise');
 
-describe('git path', function() {
-
-	it('ls', function() {
-		return exec('ls').then(function(result) {
+describe('git path', function () {
+	it('ls', function () {
+		return exec('ls').then(function (result) {
 			result = result[0].split(/\r?\n/).filter(Boolean).sort();
 			assert.ok(result.indexOf('package.json') >= 0);
 			assert.ok(result.indexOf('README.md') >= 0);
 		});
 	});
-	it('cat README.md', function() {
+	it('cat README.md', function () {
 		return Promise.all([
 			exec.cat(['README.md']),
-			fs.readFile('README.md', {encoding:'utf8'}),
-		]).then(function(result) {
+			fs.readFile('README.md', {encoding: 'utf8'}),
+		]).then(function (result) {
 			assert.equal(result[0][0], result[1]);
 		});
 	});
-	it('zdiff --help', function() {
-		return exec('zdiff', ['--help']).then(function(result) {
+	it('zdiff --help', function () {
+		return exec('zdiff', ['--help']).then(function (result) {
 			assert.ok(result[0].indexOf('OPTIONs are the same as for') >= 0);
 		});
 	});
 });
 
-describe('npm run', function() {
-
-	it('eslint --help', function() {
-		return exec('eslint', ['--help']).then(function(result) {
+describe('npm run', function () {
+	it('eslint --help', function () {
+		return exec('eslint', ['--help']).then(function (result) {
 			assert.ok(result[0].indexOf('Basic configuration:') >= 0);
 		});
 	});
-
 });
